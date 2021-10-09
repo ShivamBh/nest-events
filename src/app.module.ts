56c26ca1dadedcee89a2545018aa880c49a04f6e
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-10-09 17:27:08 +0530

    basic TDD testing controller and service mock

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
+import { SpaceShipModule } from './space-ship/space-ship.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule],
+  imports: [ProductsModule, OrdersModule, SpaceShipModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/space-ship/space-ship.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Post } from '@nestjs/common';
+import { SpaceShipService } from './space-ship.service';
+
+@Controller('space-ship')
+export class SpaceShipController {
+  constructor(private service: SpaceShipService) {}
+
+  @Post()
+  public save(spaceShip: any) {
+    this.service.save(spaceShip);
+  }
+}
